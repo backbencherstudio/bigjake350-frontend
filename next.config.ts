1d5e0ad981ffa22783fa5fafe8ab27b2d03e03dd
Md Anamul Hasan <anamul15-3625@diu.edu.bd>
2025-06-14 10:14:02 +0600

    issus solve

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   poweredByHeader: false,
-  output: 'export',
   images: {
     unoptimized: true
   }
